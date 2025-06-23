78d5551575da6e2b0b9bc5b5d89239b1dd62ba2d
jnbreid <56651766+jnbreid@users.noreply.github.com>
2025-06-23 23:04:51 +0000

    added reflector

---
+++ enigma/presets/ReflectorPresets.cpp
@@ -0,0 +1,12 @@
+
+
+#include "ReflectorPresets.h"
+
+namespace ReflectorPresets {
+
+const std::unordered_map<std::string, std::string> STANDARD_REFLECTORS = {
+    {"B", "YRUHQSLDPXNGOKMIEBFZCWVJAT"},
+    {"C", "FVPJIAOYEDRZXWGCTKUQSBNMHL"}
+};
+
+}
---
+++ enigma/presets/ReflectorPresets.h
@@ -0,0 +1,8 @@
+
+#pragma once
+#include <string>
+#include <unordered_map>
+
+namespace ReflectorPresets {
+    extern const std::unordered_map<std::string, std::string> STANDARD_REFLECTORS;
+}
