55dab8d510e946e73cff67e3acddcbdaa789ce50
jnbreid <56651766+jnbreid@users.noreply.github.com>
2025-06-24 16:49:53 +0000

    added bindings for customization

--- bindings.cpp
+++ bindings.cpp
@@ -9,4 +9,9 @@ PYBIND11_MODULE(enigma, m) {
         .def(py::init<>())
         .def("encode", &EnigmaMachine::encode)
         .def("decode", &EnigmaMachine::decode);
+        .def("set_plugboard", &EnigmaMachine::setPlugboard)
+        .def("set_reflector", &EnigmaMachine::setReflector)
+        .def("set_rotors", &EnigmaMachine::setRotors)
+        .def("set_custom_rotors", &EnigmaMachine::setCustomRotors)
+        .def("set_custom_alphabet", &EnigmaMachine::setCustomAlphabet);
 }
